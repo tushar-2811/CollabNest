4536c1552ca83a95480df90d7f727cd9c9e14955
tushar-2811 <tusharrawat52@gmail.com>
2024-12-04 19:39:58 +0530

    add types for auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -67,8 +67,8 @@ export const authOptions: NextAuthOptions = {
         if(token){
             session.user.id = token.id;
             session.user.name = token.name;
-            session.user?.email = token.email;
-            session.user?.image = token.picture;
+            session.user.email = token.email;
+            session.user.image = token.picture;
             session.user.username = token.username;
         }
 
@@ -79,13 +79,13 @@ export const authOptions: NextAuthOptions = {
         });
 
         if(user){
-            session.user?.image = user.image;
+            session.user.image = user.image;
             session.user.name = user.name.toLowerCase();
         }
 
         return session;
     },
-    async jwt(token , user){
+    async jwt({token , user}){
         const dbUser = await db.user.findFirst({
             where : {
                 email : token.email
---
+++ types/auth.ts
@@ -0,0 +1,18 @@
+import { User } from "next-auth";
+
+
+declare module "next-auth/jwt" {
+    interface JWT {
+        id : string;
+        username ?: string | null;
+    }
+}
+
+declare module "next-auth" {
+    interface Session {
+        user : User & {
+            id : string;
+            username ?: string| null;
+        }
+    }
+}
